a6988ffafb87544a1378d141b56502f15a61b403
Stephen Adeniji <praiseadeniji2017@gmail.com>
2025-02-06 00:44:35 +0100

    connect amazon

---
+++ app/(auth)/connect-amazon/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ConnectAmazon = () => {
+  return (
+    <section className="bg-[#FAFAFA] h-screen flex flex-col items-center px-4 md:px-0">
+      <div className="w-full max-w-[480px] sm:w-[480px] flex flex-col gap-4 p-6 px-3 sm:p-6 bg-white my-auto rounded-lg shadow-md">
+        <h2 className="text-center">Connect Amazon Seller Central</h2>
+      </div>
+    </section>
+  );
+};
+
+export default ConnectAmazon;
