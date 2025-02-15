40727a842a22248468380198fd4281294322c92d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2025-02-16 00:06:30 +0100

    feat: pricing page

---
+++ app/(external)/pricing/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+import Pricing from "./_components/Pricing";
+
+export const metadata: Metadata = {
+  title: "Our Pricing",
+  description: "Pricing Plans",
+};
+
+const page = () => {
+  return <Pricing />;
+};
+
+export default page;
