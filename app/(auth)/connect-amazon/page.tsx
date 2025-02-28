be98c9b94f5974d01c84bb57b7ab0db94d076bd5
Simeon ehioze <simeonehioze@gmail.com>
2025-02-28 16:49:13 +0100

    update: amazon  button redirect

--- app/(auth)/connect-amazon/page.tsx
+++ app/(auth)/connect-amazon/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from "react";
-import { signIn } from "next-auth/react";
+
+import Link from "next/link";
 
 
 
@@ -9,9 +10,12 @@ const ConnectAmazon = () => {
   return (
     <section className="">
       <div >
+        <Link href={"https://sellercentral.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.9878d848-7196-4832-9fd2-5b5796ea88c5&state=ourauth&version=beta&response_type=code&scope=sellingpartnerapi%3A%3Aauthorization&redirect_uri=https%3A%2F%2Fstaging.optisage.ai%2Famazon%2Fcallback"}>
         <button className="text-center w-full"
-       onClick={() => signIn("amazon")}
-        >Connect Amazon Seller Central</button>
+        >
+          Connect Amazon Seller Central
+        </button>
+        </Link>
       </div>
     </section>
   );
