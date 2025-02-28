9a298a5a567eeae593ddd825e420bda9e05d6114
Stephen Adeniji <praiseadeniji2017@gmail.com>
2025-02-28 14:14:53 +0100

    Update subscriptionApi.ts

--- redux/api/subscriptionApi.ts
+++ redux/api/subscriptionApi.ts
@@ -1,4 +1,5 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { baseQueryForAuth } from '../queryInterceptor';
 
 interface SubscriptionRequest {
   email:string,
@@ -15,7 +16,7 @@ interface SubscriptionResponse {
 
 export const subscriptionApi = createApi({
   reducerPath: 'subscriptionApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api-staging.optisage.ai/api' }),
+  baseQuery: baseQueryForAuth,
   endpoints: (builder) => ({
     createSubscription: builder.mutation<SubscriptionResponse, SubscriptionRequest>({
       query: (body) => ({
