9ee7a94a0f09af4d8969ece6e3712705c1b614d6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2025-03-22 16:20:51 +0100

    fix: layout shift

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useLazyGetProfileQuery } from "@/redux/api/auth";
 import { DashNav, DashSider } from "./_components";
 import { useEffect, useState } from "react";
@@ -16,34 +16,35 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [getProfile, {  }] = useLazyGetProfileQuery();
+  const [getProfile, {}] = useLazyGetProfileQuery();
   const [messageApi, contextHolder] = message.useMessage();
   //const dispatch = useDispatch();
   const router = useRouter();
-// State to manage modal visibility
-const [isModalVisible, setIsModalVisible] = useState(false);
+  // State to manage modal visibility
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-const handleLogout = () => {
-  // Clear the token cookie
-  Cookies.remove("optisage-token");
-  router.push("/");
-};
+  const handleLogout = () => {
+    // Clear the token cookie
+    Cookies.remove("optisage-token");
+    router.push("/");
+  };
 
   useEffect(() => {
     getProfile({})
       .unwrap()
       .then((res) => {
-         
-        if (res?.data?.is_subscribed === false && res?.data?.is_trial_expired === true) {
+        if (
+          res?.data?.is_subscribed === false &&
+          res?.data?.is_trial_expired === true
+        ) {
           setIsModalVisible(true);
         }
-          
       })
       .catch(() => {
         messageApi.error("failed to get Profile");
       });
-      /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [ setIsModalVisible, getProfile]);
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [setIsModalVisible, getProfile]);
   return (
     <div className="drawer lg:drawer-open text-black mx-auto max-w-screen-2xl 2xl:border">
       {contextHolder}
@@ -51,40 +52,49 @@ const handleLogout = () => {
         Toggle Drawer
       </label>
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content bg-white">
+      <div className="drawer-content bg-white max-h-dvh overflow-y-scroll">
         <DashNav />
         <div className="p-5 lg:p-6">
           {children}
 
           <Modal
-           title="Subscription Alert"
-           open={isModalVisible}
-           footer={null}
-           maskClosable={false}
-           closable={false}
-           centered={true}
+            title="Subscription Alert"
+            open={isModalVisible}
+            footer={null}
+            maskClosable={false}
+            closable={false}
+            centered={true}
           >
             <div className=" space-y-5">
-            <div className=" flex justify-center">
-            <GoAlert size={60} color="orange"/>
-            </div>
+              <div className=" flex justify-center">
+                <GoAlert size={60} color="orange" />
+              </div>
 
-            <div className=" text-center">
-              <h1 className=" font-semibold">Please be informed that your subscription has expired. To continue enjoying uninterrupted access to our services, please renew your subscription as soon as possible.</h1>
-            </div>
-            <div className=" grid grid-cols-2 gap-10">
-              <button className="px-4 py-2 bg-gray-300 rounded-lg font-bold" 
-              onClick={()=>handleLogout}>
-                Log Out
-              </button>
+              <div className=" text-center">
+                <h1 className=" font-semibold">
+                  Please be informed that your subscription has expired. To
+                  continue enjoying uninterrupted access to our services, please
+                  renew your subscription as soon as possible.
+                </h1>
+              </div>
+              <div className=" grid grid-cols-2 gap-10">
+                <button
+                  className="px-4 py-2 bg-gray-300 rounded-lg font-bold"
+                  onClick={() => handleLogout}
+                >
+                  Log Out
+                </button>
 
-              <Link href={"/renewSubscription"} className=" hover:text-white text-center px-4 py-2 bg-green-500 text-white rounded-lg font-bold">
-                Subscribe Now
-              </Link>
-            </div>
+                <Link
+                  href={"/renewSubscription"}
+                  className=" hover:text-white text-center px-4 py-2 bg-green-500 text-white rounded-lg font-bold"
+                >
+                  Subscribe Now
+                </Link>
+              </div>
             </div>
           </Modal>
-          </div>
+        </div>
       </div>
       <DashSider />
     </div>
