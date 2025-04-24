ee84c92eaad8851214613ca6dd13bda33f3c9102
Stephen <praiseadeniji2017@gmail.com>
2025-04-24 20:19:43 +0100

    feat: new seller products endpoint

--- redux/api/sellerApi.ts
+++ redux/api/sellerApi.ts
@@ -1,7 +1,6 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { baseQueryForAuth } from "../queryInterceptor";
 
-
 export const sellerApi = createApi({
   reducerPath: "Seller",
   refetchOnReconnect: true,
@@ -9,27 +8,32 @@ export const sellerApi = createApi({
   baseQuery: baseQueryForAuth,
   tagTypes: [""],
   endpoints: (builder) => ({
-
-   
     getSellerDetails: builder.query({
-      query: ({seller_id,id}) => ({
+      query: ({ seller_id, id }) => ({
         url: `sellers/${seller_id}?marketplaceId=${id}`,
         method: "GET",
       }),
     }),
     getSellerProducts: builder.query({
-      query: (params) => ({
-        url: `catalog`,
+      query: ({ sellerId, ...params }) => ({
+        url: `sellers/${sellerId}/products`,
         method: "GET",
-        params
+        params: {
+          ...params,
+          sellerId,
+        },
       }),
     }),
-
+    // getSellerProducts: builder.query({
+    //   query: (params) => ({
+    //     url: `catalog`,
+    //     method: "GET",
+    //     params,
+    //   }),
+    // }),
   }),
 });
 
-export const {
-useLazyGetSellerDetailsQuery,
-useLazyGetSellerProductsQuery
+export const { useLazyGetSellerDetailsQuery, useLazyGetSellerProductsQuery } =
+  sellerApi;
 
-} = sellerApi;
