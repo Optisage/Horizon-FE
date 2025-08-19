1fd51dccfed5532368639ce8af0f0671ca965535
simziky <simeonehioze@gmail.com>
2025-08-19 23:15:38 +0100

    update:google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-//import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/redux/provider";
@@ -9,21 +8,10 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "@ant-design/v5-patch-for-react-19";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useRouter } from "next/navigation";
-
+import Script from "next/script"; 
 import Cookies from "js-cookie";
 
 const inter = Inter({ subsets: ["latin"] });
-
-/** 
- const metadata: Metadata = {
-  title: {
-    template: "%s | optisage",
-    default: "Sign In",
-  },
-  description: "Please Sign in to continue to your account",
-  keywords: ["optisage"],
-};
-*/
 
 export default function RootLayout({
   children,
@@ -46,7 +34,29 @@ export default function RootLayout({
 
   return (
     <html lang="en" data-theme="light">
+      <head>
+        
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TZHW7GNM');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} antialiased`}>
+        
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TZHW7GNM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <AntdRegistry>
           <GoogleOAuthProvider
             clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}
@@ -58,4 +68,3 @@ export default function RootLayout({
     </html>
   );
 }
-
