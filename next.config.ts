6bc2b0c3e81ad037d035d4f68ee3fb06cd19c511
Nurudeen <99297818+Nurdynzo@users.noreply.github.com>
2025-09-15 11:44:14 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   productionBrowserSourceMaps: true,
+  output: "standalone",
   images: {
     remotePatterns: [{
       protocol: "https",
